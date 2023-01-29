c4dd4a88e8f928185574414c7a6b6c94dc56bb1c
Sam <sams421@icloud.com>
2023-01-29 18:53:59 +0000

    Fixed duplicate messages being sent

--- chatroom_project/settings.py
+++ chatroom_project/settings.py
@@ -23,13 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = key
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG=True
+ALLOWED_HOSTS=['*']
 
-ALLOWED_HOSTS = [
-    'quickchat.pythonanywhere.com',
-    '.pythonanywhere.com'
-]
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False
+
+# ALLOWED_HOSTS = [
+#     'quickchat.pythonanywhere.com',
+#     '.pythonanywhere.com'
+# ]
 
 # Application definition
 
